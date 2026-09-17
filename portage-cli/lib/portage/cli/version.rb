7fade55aa6b0d9a290dd46df679de683437b62c2
Tom Whitbread <tom@gript.co.uk>
2026-09-17 11:40:43 +0700

    Bump portage-ucp, portage-cli, portage-ucp-client, and every adapter to their 0.8.0-line versions

--- portage-cli/lib/portage/cli/version.rb
+++ portage-cli/lib/portage/cli/version.rb
@@ -1,5 +1,5 @@
 module Portage
   module Cli
-    VERSION = "0.5.1".freeze
+    VERSION = "0.6.0".freeze
   end
 end
--- portage-ucp-journal/lib/portage/ucp/journal/version.rb
+++ portage-ucp-journal/lib/portage/ucp/journal/version.rb
@@ -1,7 +1,7 @@
 module Portage
   module Ucp
     module Journal
-      VERSION = "0.1.0".freeze
+      VERSION = "0.1.1".freeze
     end
   end
 end
